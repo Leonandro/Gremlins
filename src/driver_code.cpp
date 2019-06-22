2ed12990d3a4bd3aa2f58dc2a665d2c89c33a79f
Leonandro <leonandrogurgel@gmail.com>
2019-06-22 18:12:44 -0300

    Pequeno teste de eficiência versus o SO

--- src/driver_code.cpp
+++ src/driver_code.cpp
@@ -5,9 +5,28 @@
 
 int main ()
 {
-    SLPool <16> arena (220);
-    int * ptr = (int *)arena.Allocate(4);
-    *ptr = 4;
-    std::cout << "PTR = " << *ptr /* <<  " - " << *hue <<*/ << std::endl;
-    arena.print();
+    SLPool <16> arena (5000000);
+    int * test_ptr = nullptr;
+    double tempos = 0;
+    for(int i = 0; i < (200); i++)
+    {
+        std::chrono::steady_clock::time_point _start(std::chrono::steady_clock::now()); 
+        test_ptr = (int *)arena.Allocate(4);
+        std::chrono::steady_clock::time_point _end(std::chrono::steady_clock::now());
+        tempos += std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(_end - _start).count(); 
+    }
+    std::cout << "Tempo total para 200 alocações no SLpool: " << tempos 
+              << " Média por alocação: " << tempos/200 << std::endl;
+
+    test_ptr = nullptr;
+    tempos = 0;
+    for(int i = 0; i < (200); i++)
+    {
+        std::chrono::steady_clock::time_point _start(std::chrono::steady_clock::now()); 
+        test_ptr = new int[1];
+        std::chrono::steady_clock::time_point _end(std::chrono::steady_clock::now());
+        tempos += std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(_end - _start).count(); 
+    }
+    std::cout << "Tempo total para 200 alocações no Sistema Operacional: " << tempos 
+              << " Média por alocação: " << tempos/200 << std::endl;
 }
